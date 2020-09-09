1a5a2fad9677796d71532df8630a2d451d1e34f6
camshaft54 <cam.shaw1234@gmail.com>
2020-09-09 10:46:40 -0700

    Add config

--- src/main/java/io/github/camshaft54/idlebot/IdleBot.java
+++ src/main/java/io/github/camshaft54/idlebot/IdleBot.java
@@ -2,6 +2,7 @@ package io.github.camshaft54.idlebot;
 
 import io.github.camshaft54.idlebot.events.IdleBotEvents;
 import io.github.camshaft54.idlebot.events.IdleChecker;
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.javacord.api.DiscordApi;
 import org.javacord.api.DiscordApiBuilder;
@@ -9,25 +10,37 @@ import org.javacord.api.entity.channel.ServerTextChannel;
 
 public class IdleBot extends JavaPlugin {
     public static ServerTextChannel channel;
+    FileConfiguration config = this.getConfig();
+    private static String botToken;
+    private static String channelId;
 
     @Override
     public void onEnable() {
-        getLogger().info("[IdleBot]: THIS PLUGIN WORKS!!!");
+        configSetup();
         // Connect to Discord
         DiscordApi api = new DiscordApiBuilder()
-                .setToken("NzUyOTE5OTMxMzc2MzY5NzE0.X1epVA.MOQxOQt8BoRmn8nV_zwgf4AGJMg") // Set the token of the bot here
+                .setToken(botToken) // Set the token of the bot here
                 .login() // Log the bot in
                 .join(); // Call #onConnectToDiscord(...) after a successful login
         getLogger().info("Connected to Discord as " + api.getYourself().getDiscriminatedName());
         getLogger().info("Open the following url to invite the bot: " + api.createBotInvite());
 
-        if (api.getServerTextChannelById("752921018150027404").isPresent()) {
-            channel = api.getServerTextChannelById("752921018150027404").get();
+        if (api.getServerTextChannelById(channelId).isPresent()) {
+            channel = api.getServerTextChannelById(channelId).get();
         }
         else {
             getLogger().info("Channel not present");
         }
         getServer().getScheduler().runTaskTimer(this, new IdleChecker(), 20L, 20L); // Code in task should execute every 20 ticks (1 second)
         getServer().getPluginManager().registerEvents(new IdleBotEvents(), this);
+    }
+
+    private void configSetup() {
+        config.addDefault("botToken", "<Bot Token Here>");
+        config.addDefault("channelId", "<Channel Id Here>");
+        config.options().copyDefaults(true);
+        saveConfig();
+        botToken = config.getString("botToken");
+        channelId = config.getString("channelId");
     }
 }
