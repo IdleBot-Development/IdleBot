73d02f2d549e8a762d3f3497b51aa85d8e537d69
camshaft54 <cam.shaw1234@gmail.com>
2020-09-09 18:00:57 -0700

    Add location message

--- src/main/java/io/github/camshaft54/idlebot/events/IdleBotEvents.java
+++ src/main/java/io/github/camshaft54/idlebot/events/IdleBotEvents.java
@@ -2,6 +2,7 @@ package io.github.camshaft54.idlebot.events;
 
 import io.github.camshaft54.idlebot.IdleBot;
 import org.bukkit.Bukkit;
+import org.bukkit.Location;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -16,7 +17,7 @@ public class IdleBotEvents implements Listener {
         if (IdleChecker.idlePlayers.contains(player)) {
             player.sendMessage("[IdleBot]: You are idle and dead!");
             Bukkit.getLogger().info("[IdleBot]: " + player.getDisplayName() + " is idle and dead!");
-            IdleBot.channel.sendMessage("Player Died.");
+            IdleBot.channel.sendMessage(player.getDisplayName() + " died at " + locationCleanup(player.getLocation()) + " (" + e.getDeathMessage() + ").");
         }
         else {
             Bukkit.getLogger().info("some player died.");
@@ -28,6 +29,10 @@ public class IdleBotEvents implements Listener {
         Player player = e.getPlayer();
         Bukkit.getLogger().info(player.getDisplayName() + " stopped being idle.");
         IdleChecker.playersIdle.put(player,0);
+    }
+
+    public static String locationCleanup(Location l) {
+        return "(" + Math.round(l.getX()) + ", " + Math.round(l.getY()) + ", " + Math.round(l.getZ()) + ")";
     }
 }
 
