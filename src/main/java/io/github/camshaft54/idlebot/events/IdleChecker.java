665acf63f3d4cba3e4e6d51426194f6ac3ae0c91
camshaft54 <cam.shaw1234@gmail.com>
2020-09-11 21:29:25 -0700

    Fix idle checker

--- src/main/java/io/github/camshaft54/idlebot/events/IdleChecker.java
+++ src/main/java/io/github/camshaft54/idlebot/events/IdleChecker.java
@@ -1,7 +1,6 @@
 package io.github.camshaft54.idlebot.events;
 
 import io.github.camshaft54.idlebot.IdleBot;
-import io.github.camshaft54.idlebot.User;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 
@@ -15,24 +14,22 @@ public class IdleChecker implements Runnable {
 
     public void run() {
         for (Player player : Bukkit.getOnlinePlayers()) {
-            for (User user : IdleBot.users.values()) {
-                if (!player.getUniqueId().toString().equals(user.getMCName())) {
-                    continue;
-                }
-                if (playersIdling.containsKey(player)) { // If player is already in the hashmap
-                    playersIdling.put(player, playersIdling.get(player) + 1); // Add 1 to the player's value
-                    Bukkit.getLogger().info("player had been idle for " + playersIdling.get(player));
-                } else { // If not
-                    // Put new player to the hashmap with value 1 (1 second)
-                    playersIdling.put(player, 0);
-                    IdleBotEvents.isDamaged.put(player, false);
-                    Bukkit.getLogger().info("A new player joined");
-                }
-                if (playersIdling.get(player) == 30) { // If player's value is 120 (120 seconds)
-                    player.sendMessage("You are idle!!!");
-                    Bukkit.getLogger().info("[IdleBot]: " + player.getDisplayName() + " is idle!!!");
-                    idlePlayers.add(player);
-                }
+            if (!IdleBot.users.containsKey(player.getUniqueId().toString())) {
+                continue;
+            }
+            if (playersIdling.containsKey(player)) { // If player is already in the hashmap
+                playersIdling.put(player, playersIdling.get(player) + 1); // Add 1 to the player's value
+                Bukkit.getLogger().info("player had been idle for " + playersIdling.get(player));
+            } else { // If not
+                // Put new player to the hashmap with value 1 (1 second)
+                playersIdling.put(player, 0);
+                IdleBotEvents.isDamaged.put(player, false);
+                Bukkit.getLogger().info("A new player joined");
+            }
+            if (playersIdling.get(player) == 30) { // If player's value is 120 (120 seconds)
+                player.sendMessage("You are idle!!!");
+                Bukkit.getLogger().info("[IdleBot]: " + player.getDisplayName() + " is idle!!!");
+                idlePlayers.add(player);
             }
         }
         Bukkit.getLogger().info("[IdleBot]: " + "Hashmap:" + playersIdling.keySet().toString() + "Values: " + playersIdling.values().toString());
