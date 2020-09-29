624a200bed100900bb8c4822f42791e2875128fa
camshaft54 <cam.shaw1234@gmail.com>
2020-09-29 09:45:14 -0700

    Update dependency for spigot api NOTE: Javacord 3.1.0 does not work, need to fix

--- src/main/java/io/github/camshaft54/idlebot/events/IdleBotEvents.java
+++ src/main/java/io/github/camshaft54/idlebot/events/IdleBotEvents.java
@@ -17,6 +17,34 @@ import java.util.HashMap;
 public class IdleBotEvents implements Listener {
     public static HashMap<Player, Boolean> isDamaged = new HashMap<>();
 
+    // If player has moved, reset playersIdling and the other condition hashmaps
+    @EventHandler
+    public static void onMovement(PlayerMoveEvent e) {
+        Player player = e.getPlayer();
+        Bukkit.getLogger().info(player.getDisplayName() + " stopped being idle.");
+        IdleChecker.playersIdling.put(player, 0);
+        isDamaged.put(player, false);
+        IdleChecker.isFull.put(player, false);
+        IdleChecker.atExpLevel.put(player, false);
+        IdleChecker.atLocation.put(player, false);
+    }
+
+    // If player leaves game, remove them from playersIdling and the other condition hashmaps
+    @EventHandler
+    public static void onPlayerQuit(PlayerQuitEvent e) {
+        Player player = e.getPlayer();
+        IdleChecker.playersIdling.remove(player);
+        IdleChecker.isFull.remove(player);
+        IdleChecker.atExpLevel.remove(player);
+        isDamaged.remove(player);
+    }
+
+    // Take player's location and return a clean string of coordinates
+    private static String locationCleanup(Location l) {
+        return "(" + Math.round(l.getX()) + ", " + Math.round(l.getY()) + ", " + Math.round(l.getZ()) + ")";
+    }
+
+    // If player has died, send them a message
     @EventHandler
     public void onDeath(PlayerDeathEvent e) {
         Player player = e.getEntity();
@@ -30,6 +58,7 @@ public class IdleBotEvents implements Listener {
         }
     }
 
+    // If player was damaged, send them a message
     @EventHandler
     public void onDamage(EntityDamageEvent e) {
         if (e.getEntity() instanceof Player) {
@@ -44,29 +73,6 @@ public class IdleBotEvents implements Listener {
                 }
             }
         }
-    }
-
-    @EventHandler
-    public static void onMovement(PlayerMoveEvent e) {
-        Player player = e.getPlayer();
-        Bukkit.getLogger().info(player.getDisplayName() + " stopped being idle.");
-        IdleChecker.playersIdling.put(player, 0);
-        isDamaged.put(player, false);
-        IdleChecker.isFull.put(player, false);
-        IdleChecker.atExpLevel.put(player, false);
-    }
-
-    @EventHandler
-    public static void onPlayerQuit(PlayerQuitEvent e) {
-        Player player = e.getPlayer();
-        IdleChecker.playersIdling.remove(player);
-        IdleChecker.isFull.remove(player);
-        IdleChecker.atExpLevel.remove(player);
-        isDamaged.remove(player);
-    }
-
-    private static String locationCleanup(Location l) {
-        return "(" + Math.round(l.getX()) + ", " + Math.round(l.getY()) + ", " + Math.round(l.getZ()) + ")";
     }
 }
 
--- src/main/java/io/github/camshaft54/idlebot/events/IdleChecker.java
+++ src/main/java/io/github/camshaft54/idlebot/events/IdleChecker.java
@@ -11,34 +11,9 @@ public class IdleChecker implements Runnable {
     public static HashMap<Player, Integer> playersIdling = new HashMap<>();
     public static HashMap<Player, Boolean> isFull = new HashMap<>();
     public static HashMap<Player, Boolean> atExpLevel = new HashMap<>();
+    public static HashMap<Player, Boolean> atLocation = new HashMap<>();
 
-    public void run() {
-        IdleBot.getUsers();
-        for (Player player : Bukkit.getOnlinePlayers()) {
-            if (!IdleBot.users.containsKey(player.getUniqueId().toString())) {
-                continue;
-            }
-            if (playersIdling.containsKey(player)) { // If player is already in the hashmap
-                playersIdling.put(player, playersIdling.get(player) + 1); // Add 1 to the player's value
-                Bukkit.getLogger().info("player had been idle for " + playersIdling.get(player));
-            } else { // If not
-                // Put new player to the hashmap with value 1 (1 second)
-                playersIdling.put(player, 0);
-                IdleBotEvents.isDamaged.put(player, false);
-                isFull.put(player, false);
-                atExpLevel.put(player, false);
-                Bukkit.getLogger().info("A new player joined");
-            }
-            if (playersIdling.get(player) == IdleBot.idleTime) { // If player has been idle for time specified in config
-                player.sendMessage("You are idle!!!");
-                Bukkit.getLogger().info("[IdleBot]: " + player.getDisplayName() + " is idle!!!");
-            }
-        }
-        Bukkit.getLogger().info("[IdleBot]: " + "Hashmap:" + playersIdling.keySet().toString() + "Values: " + playersIdling.values().toString());
-        inventoryFull();
-        xpLevel();
-    }
-
+    // Checks if players inventory is full and sends them a message if it is
     private static void inventoryFull() {
         for (Player player : playersIdling.keySet()) {
             String playerId = player.getUniqueId().toString();
@@ -51,16 +26,59 @@ public class IdleChecker implements Runnable {
         }
     }
 
+    // Checks if player has reached a certain xp level and sends them a message if they have
     private static void xpLevel() {
         for (Player player : playersIdling.keySet()) {
             String playerId = player.getUniqueId().toString();
             String discordId = IdleBot.users.get(playerId).getDiscordId();
-            //TODO: Replace "10" in if statement with variable for desired Exp level set by player
+            //TODO: Replace "10" in if statement with variable for desired xp level set by player
             if (playersIdling.get(player) >= IdleBot.idleTime && player.getLevel() == 10 && discordId != null && !atExpLevel.get(player)) {
                 Bukkit.getLogger().info("[IdleBot]: " + player.getDisplayName() + " is idle and at the desired XP level!");
                 IdleBot.channel.sendMessage("<@!" + discordId + "> " + player.getDisplayName() + " is at the desired XP level! ");
                 atExpLevel.put(player, true);
             }
         }
+    }
+
+    // Checks if a player has reached a certain location and sends them a message if they have
+    private static void locationReached() {
+        for (Player player : playersIdling.keySet()) {
+            String playerId = player.getUniqueId().toString();
+            String discordId = IdleBot.users.get(playerId).getDiscordId();
+            // TODO: Replace "100"s with the XYZ coordinates with variable for desired location set by player
+            boolean reachedLocation = player.getLocation().getBlockX() == 100 && player.getLocation().getBlockY() == 100 && player.getLocation().getBlockZ() == 100;
+            if (playersIdling.get(player) >= IdleBot.idleTime && reachedLocation && discordId != null && !atLocation.get(player)) {
+                Bukkit.getLogger().info("[IdleBot]: " + player.getDisplayName() + " is idle and they reached their desired location!");
+                IdleBot.channel.sendMessage("<@!" + discordId + "> " + player.getDisplayName() + "'s reached the desired location! ");
+                atLocation.put(player, true);
+            }
+        }
+    }
+
+    public void run() {
+        IdleBot.getUsers();
+        for (Player player : Bukkit.getOnlinePlayers()) {
+            if (!IdleBot.users.containsKey(player.getUniqueId().toString())) {
+                continue;
+            }
+            if (playersIdling.containsKey(player)) { // If player is already in the hashmap
+                playersIdling.put(player, playersIdling.get(player) + 1); // Add 1 to the player's value
+            } else { // If not
+                // Put new player into the playersIdling hashmap with value 0 (0 seconds)
+                playersIdling.put(player, 0);
+                // Add player to the various condition hashmaps
+                IdleBotEvents.isDamaged.put(player, false);
+                isFull.put(player, false);
+                atExpLevel.put(player, false);
+                atLocation.put(player, false);
+            }
+            if (playersIdling.get(player) == IdleBot.idleTime) { // If player has been idle for time specified in config
+                Bukkit.getLogger().info("[IdleBot]: " + player.getDisplayName() + " is idle.");
+            }
+        }
+        // Run methods to check for certain conditions
+        inventoryFull();
+        xpLevel();
+        locationReached();
     }
 }
