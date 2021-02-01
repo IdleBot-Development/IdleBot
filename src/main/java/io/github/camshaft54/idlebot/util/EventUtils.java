2bc93d24b59b1a4a7c71dc9e92b6c43c1adbfa84
MetalTurtle <dekolis1218@gmail.com>
2021-01-31 18:25:33 -0800

    Add license information to every file and update copyright to 2021

--- src/main/java/io/github/camshaft54/idlebot/util/EventUtils.java
+++ src/main/java/io/github/camshaft54/idlebot/util/EventUtils.java
@@ -1,5 +1,5 @@
 /*
- *    Copyright (C) 2020 Camshaft54, MetalTurtle18
+ *    Copyright (C) 2020-2021 Camshaft54, MetalTurtle18
  *
  *    This program is free software: you can redistribute it and/or modify
  *    it under the terms of the GNU General Public License as published by
