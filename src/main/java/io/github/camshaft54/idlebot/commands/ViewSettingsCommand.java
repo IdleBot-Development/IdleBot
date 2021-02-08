abe4519d28468159278c5d70fb9cf1a25e65d81b
camshaft54 <cam.shaw1234@gmail.com>
2021-02-08 10:09:29 -0800

    Revert "Attempt to fix n-path > 200 error prone code"
    This reverts commit c2b0943aaac17f6c24a83086261352751cf3beb1.

--- src/main/java/io/github/camshaft54/idlebot/commands/ViewSettingsCommand.java
+++ src/main/java/io/github/camshaft54/idlebot/commands/ViewSettingsCommand.java
@@ -26,11 +26,10 @@ import io.github.camshaft54.idlebot.util.enums.MessageLevel;
 import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Objects;
 
 public class ViewSettingsCommand implements IdleBotCommand {
+    private Player player;
 
     @Override
     public String getCommandName() {
@@ -43,28 +42,30 @@ public class ViewSettingsCommand implements IdleBotCommand {
     }
 
     @Override
-    public boolean runCommand(Player player, String[] args) {
+    public boolean runCommand(Player p, String[] args) {
+        player = p;
         Messenger.sendMessage(player, "Your current settings: ", MessageLevel.INFO);
         String discordID = PersistentDataHandler.getStringData(player, DataValues.DISCORD_ID.key());
+        sendAquaMessage("Account linked: " + (discordID == null ? "false" : ("true (" + Objects.requireNonNull(DiscordAPIManager.bot.retrieveUserById(discordID)).complete().getAsTag() + ")")));
+        sendAquaMessage("Message channel: " + (PersistentDataHandler.getBooleanData(player, DataValues.DIRECT_MESSAGE_MODE.key()) ? "direct message" : "public channel"));
+        sendAquaMessage("AFK mode: " + (PersistentDataHandler.getBooleanData(player, DataValues.AUTO_AFK.key()) ? "auto" : ("manual (Set AFK: " + PersistentDataHandler.getBooleanData(player, DataValues.IS_SET_AFK.key()) + ")")));
+        sendAquaMessage("AFK time: " + PersistentDataHandler.getIntData(player, DataValues.AFK_TIME.key()));
+        sendAquaMessage("Damage alert: " + PersistentDataHandler.getBooleanData(player, DataValues.DAMAGE_ALERT.key()));
+        sendAquaMessage("Death alert: " + PersistentDataHandler.getBooleanData(player, DataValues.DEATH_ALERT.key()));
         boolean locationAlertX = PersistentDataHandler.getBooleanData(player, DataValues.LOCATION_ALERT_X.key());
         int locationX = PersistentDataHandler.getIntData(player, DataValues.LOCATION_X_DESIRED.key());
+        sendAquaMessage("Location alert (X): " + locationAlertX + (locationAlertX ? (locationX == -1 ? " (no location set)" : (" (" + locationX + ")")) : ""));
         boolean locationAlertZ = PersistentDataHandler.getBooleanData(player, DataValues.LOCATION_ALERT_Z.key());
         int locationZ = PersistentDataHandler.getIntData(player, DataValues.LOCATION_Z_DESIRED.key());
+        sendAquaMessage("Location alert (Z): " + locationAlertZ + (locationAlertZ ? (locationZ == -1 ? " (no location set)" : (" (" + locationZ + ")")) : ""));
         boolean xpAlert = PersistentDataHandler.getBooleanData(player, DataValues.EXPERIENCE_ALERT.key());
         int xpLevel = PersistentDataHandler.getIntData(player, DataValues.EXPERIENCE_LEVEL_DESIRED.key());
-        ArrayList<String> settingsMsg  = new ArrayList<>(Arrays.asList(
-                "Account linked: " + (discordID == null ? "false" : ("true (" + Objects.requireNonNull(DiscordAPIManager.bot.retrieveUserById(discordID)).complete().getAsTag() + ")")),
-                "Message channel: " + (PersistentDataHandler.getBooleanData(player, DataValues.DIRECT_MESSAGE_MODE.key()) ? "direct message" : "public channel"),
-                "AFK mode: " + (PersistentDataHandler.getBooleanData(player, DataValues.AUTO_AFK.key()) ? "auto" : ("manual (Set AFK: " + PersistentDataHandler.getBooleanData(player, DataValues.IS_SET_AFK.key()) + ")")),
-                "AFK time: " + PersistentDataHandler.getIntData(player, DataValues.AFK_TIME.key()),
-                "Damage alert: " + PersistentDataHandler.getBooleanData(player, DataValues.DAMAGE_ALERT.key()),
-                "Death alert: " + PersistentDataHandler.getBooleanData(player, DataValues.DEATH_ALERT.key()),
-                "Location alert (X): " + locationAlertX + (locationAlertX ? (locationX == -1 ? " (no location set)" : (" (" + locationX + ")")) : ""),
-                "Location alert (Z): " + locationAlertZ + (locationAlertZ ? (locationZ == -1 ? " (no location set)" : (" (" + locationZ + ")")) : ""),
-                "Experience alert: " + xpAlert + (xpAlert ? (xpLevel == -1 ? " (no XP level set)" : " (" + xpLevel + ")") : ""),
-                "Inventory fill alert: " + PersistentDataHandler.getBooleanData(player, DataValues.INVENTORY_FULL_ALERT.key())));
-        String[] settingsMsgArray = settingsMsg.stream().map(s -> ChatColor.AQUA + s).toArray(String[]::new);
-        player.sendMessage(settingsMsgArray);
+        sendAquaMessage("Experience alert: " + xpAlert + (xpAlert ? (xpLevel == -1 ? " (no XP level set)" : " (" + xpLevel + ")") : ""));
+        sendAquaMessage("Inventory fill alert: " + PersistentDataHandler.getBooleanData(player, DataValues.INVENTORY_FULL_ALERT.key()));
         return true;
+    }
+
+    private void sendAquaMessage(String message) {
+        player.sendMessage(ChatColor.AQUA + message);
     }
 }
