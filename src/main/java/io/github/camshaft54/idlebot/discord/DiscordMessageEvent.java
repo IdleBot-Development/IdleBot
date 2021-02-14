bddff5bc416699b6c8268a059960e120a0d4df89
camshaft54 <cam.shaw1234@gmail.com>
2021-02-13 23:39:36 -0800

    Remove NFE stacktrace

--- src/main/java/io/github/camshaft54/idlebot/discord/DiscordMessageEvent.java
+++ src/main/java/io/github/camshaft54/idlebot/discord/DiscordMessageEvent.java
@@ -52,7 +52,6 @@ public class DiscordMessageEvent extends ListenerAdapter {
                 }
             } catch (NumberFormatException nfe) {
                 channel.sendMessage("Invalid Code. To get code type `/idlebot link` in Minecraft").queue();
-                nfe.printStackTrace();
             }
         }
     }
