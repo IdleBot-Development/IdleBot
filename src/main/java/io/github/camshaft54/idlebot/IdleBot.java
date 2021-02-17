0ee744fafa738e02e73d441b3e6e8c818ef08051
MetalTurtle <dekolis1218@gmail.com>
2021-02-16 17:23:31 -0800

    Remove unused imports and bump version to 1.1.1

--- src/main/java/io/github/camshaft54/idlebot/IdleBot.java
+++ src/main/java/io/github/camshaft54/idlebot/IdleBot.java
@@ -34,12 +34,7 @@ import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.scheduler.BukkitScheduler;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.URL;
-import java.net.URLConnection;
 import java.util.HashMap;
 import java.util.Objects;
 
